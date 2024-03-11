ab87e8c0972f6f311ac97ac86c2f25ed4f095ad4
timohavdk <timoha200120@gmail.com>
2024-03-12 01:20:32 +1000

    Верстка профиля в приложении

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'X',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +29,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.white70, brightness: Brightness.dark),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'X'),
     );
   }
 }
@@ -55,29 +57,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final String _name = 'Тимофей';
+  final String _surname = 'Андреев';
+  final String _about = '22 года, программист, кроссфитер, магистрант ДВФУ';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  void _editName() {}
 
-  void _decrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter--;
-    });
-  }
+  void _editSurName() {}
+
+  void _editAbout() {}
 
   @override
   Widget build(BuildContext context) {
@@ -92,7 +80,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
@@ -100,37 +87,116 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(onPressed: _incrementCounter, child: const Text('Плюс 1')),
-                TextButton(onPressed: _decrementCounter, child: const Text('Минус 1')),
-              ],
-            )
-          ],
+        child: ConstrainedBox(
+          constraints: BoxConstraints.expand(width: 400),
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    Icons.account_circle_outlined,
+                    color: Colors.white70,
+                    size: 60,
+                    semanticLabel: 'Account icon',
+                  ),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 0, 16.0, 0),
+                          child: Text(
+                            '$_name',
+                            style: const TextStyle(
+                              color: Colors.white70,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: _editName,
+                          child: const Icon(
+                            Icons.edit,
+                            color: Colors.white70,
+                            size: 20,
+                            semanticLabel: 'Edit name icon',
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 0, 16.0, 0),
+                          child: Text(
+                            '$_surname',
+                            style: const TextStyle(
+                              color: Colors.white70,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: _editSurName,
+                          child: const Icon(
+                            Icons.edit,
+                            color: Colors.white70,
+                            size: 20,
+                            semanticLabel: 'Edit surname icon',
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ]),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 16.0, 0),
+                      child: Text(
+                        '$_about',
+                        softWrap: true,
+                        style: const TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                          // overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: _editAbout,
+                    child: const Icon(
+                      Icons.edit,
+                      color: Colors.white70,
+                      size: 20,
+                      semanticLabel: 'Edit about icon',
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
